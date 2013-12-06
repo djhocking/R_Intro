9f25cd17ba3823a9f972bf4fa7701789ab616427
Daniel Hocking <dhocking@unh.edu>
2013-12-06 17:08:28 -0500

    moved files

--- 02_Data_Manipulation/CreatePDF.R
+++
@@ -1,7 +0,0 @@
-# Create .md, .html, and .pdf files
-library(knitr)
-library(markdown)
-
-knit("Data-Manipulation.Rmd")
-markdownToHTML('Data-Manipulation.md', 'Data-Manipulation.html', options=c("use_xhml"))
-system("pandoc -s Data-Manipulation.html -o Data-Manipulation.pdf -H margins.sty")
